ea8f1a46e91518ee520edde074e43fb2749bfe6a
santiago canal <santycanal_06@hotmail.com>
2022-12-10 23:12:57 -0500

    listo para deploy

--- backend/server/config.js
+++ backend/server/config.js
@@ -6,10 +6,11 @@ export const MONGODB_URI=process.env.MONGODB_URI || "mongodb+srv://cluster0.r0dq
 
 export const PORT=process.env.PORT || 4000
 
-export const CLOUDINARY_NAME=process.env.CLOUDINARY_NAME
+export const CLOUDINARY_NAME=process.env.CLOUDINARY_NAME || 'santiagocanal'
 
-export const CLOUDINARY_API_KEY=process.env.CLOUDINARY_API_KEY
+export const CLOUDINARY_API_KEY=process.env.CLOUDINARY_API_KEY || '626361669696184'
 
-export const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET
+
+export const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET || 'DSP7M2QLSHyFFn-fcXa6DlSApSE'
 
 
